cf9b6e46a741b7bd049a12e3f49a7e651e7d808c
eattato <eattato0804@gmail.com>
2023-08-19 23:38:13 +0900

    학교 선택 완성함

--- client/src/pages/form.js
+++ client/src/pages/form.js
@@ -1,178 +1,251 @@
-import "../pages/formBase.css"
-import "../pages/form.css"
-import React, { useState, createContext, useContext } from 'react';
-import FormContext from "../modules/FormContext"
+import "../pages/formBase.css";
+import "../pages/form.css";
+import React, { useState, createContext, useContext } from "react";
+import FormContext from "../modules/FormContext";
 
 function InputLine({ label, placeholder, value, error, type, onChange }) {
-    const change = (event) => {
-        if (!onChange) return;
-        onChange(event.target.value);
-    }
+  const change = (event) => {
+    if (!onChange) return;
+    onChange(event.target.value);
+  };
 
-    return (
-        <div className='form_line'>
-            <div className='form_line_label'>{label}</div>
-            <input className='form_line_input' placeholder={placeholder} type={type} value={value} onChange={change} />
-            <div className='form_line_error'>{error}</div>
-        </div>
-    );
+  return (
+    <div className="form_line">
+      <div className="form_line_label">{label}</div>
+      <input
+        className="form_line_input"
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={change}
+      />
+      <div className="form_line_error">{error}</div>
+    </div>
+  );
 }
 
 function InputListBox({ label, onClick }) {
-    const click = () => {
-        onClick();
-    }
+  const click = () => {
+    onClick();
+  };
 
-    return (
-        <li className={`form_line_list_box ${onClick? "clickable" : ""}`} onClick={click}>{label}</li>
-    );
+  return (
+    <li
+      className={`form_line_list_box ${onClick ? "clickable" : ""}`}
+      onClick={click}
+    >
+      {label}
+    </li>
+  );
 }
 
-function InputList({ label, placeholder, type, value, error, list, onChange, onClick }) {
-    const change = (event) => {
-        if (!onChange) return;
-        onChange(event.target.value);
-    }
+function InputList({
+  label,
+  placeholder,
+  type,
+  value,
+  error,
+  list,
+  onChange,
+  onClick,
+}) {
+  const change = (event) => {
+    if (!onChange) return;
+    onChange(event.target.value);
+  };
 
-    const click = () => {
-        if (!onClick) return;
-        onClick();
-    }
+  const click = () => {
+    if (!onClick) return;
+    onClick();
+  };
 
-    return (
-        <div className='form_line'>
-            <div className='form_line_label'>{label}</div>
-            <div className="form_line_input_frame">
-                <input className='form_line_input' placeholder={placeholder} type={type} value={value} onChange={change} />
-                <button className="form_line_input_search" type="button" onClick={click}>검색</button>
-            </div>
-            <ul className={`form_line_list ${list.length > 0 ? "active" : ""}`}>
-                {list.map((li) => <InputListBox label={li.label} onClick={li.click} />)}
-            </ul>
-            <div className='form_line_error'>{error}</div>
-        </div>
-    );
+  return (
+    <div className="form_line">
+      <div className="form_line_label">{label}</div>
+      <div className="form_line_input_frame">
+        <input
+          className="form_line_input"
+          placeholder={placeholder}
+          type={type}
+          value={value}
+          onChange={change}
+        />
+        <button
+          className="form_line_input_search"
+          type="button"
+          onClick={click}
+        >
+          검색
+        </button>
+      </div>
+      <ul className={`form_line_list ${list.length > 0 ? "active" : ""}`}>
+        {list.map((li) => (
+          <InputListBox label={li.label} onClick={li.click} />
+        ))}
+      </ul>
+      <div className="form_line_error">{error}</div>
+    </div>
+  );
 }
 
 function Form() {
-    const {grade, setGrade, name, setName, school, setSchool, setFormFilled, setStartTime} = useContext(FormContext);
-    const [gradeError, setGradeError] = useState(null);
-    const [nameError, setNameError] = useState(null);
+  const {
+    grade,
+    setGrade,
+    name,
+    setName,
+    school,
+    setSchool,
+    setFormFilled,
+    setStartTime,
+  } = useContext(FormContext);
+  const [gradeError, setGradeError] = useState(null);
+  const [nameError, setNameError] = useState(null);
 
-    const [schoolError, setSchoolError] = useState(null);
-    const [schoolLoading, setSchoolLoading] = useState(false);
-    const [schoolLock, setSchoolLock] = useState(false);
-    const [schoolList, setSchoolList] = useState([]);
+  const [schoolError, setSchoolError] = useState(null);
+  const [schoolLoading, setSchoolLoading] = useState(false);
+  const [schoolLock, setSchoolLock] = useState(false);
+  const [schoolList, setSchoolList] = useState([]);
 
-    const changeGrade = (value) => {
-        setGrade(value);
-        if (value.length == 0) {
-            setGradeError("학번을 입력하세요.");
-        } else if (value.length != 5) {
-            setGradeError("학번은 5글자 입니다.");
-        } else if (isNaN(value)) {
-            setGradeError("학번은 숫자로만 구성될 수 있습니다.");
-        } else {
-            setGradeError(null);
-            return true;
+  const changeGrade = (value) => {
+    setGrade(value);
+    if (value.length == 0) {
+      setGradeError("학번을 입력하세요.");
+    } else if (value.length != 5) {
+      setGradeError("학번은 5글자 입니다.");
+    } else if (isNaN(value)) {
+      setGradeError("학번은 숫자로만 구성될 수 있습니다.");
+    } else {
+      setGradeError(null);
+      return true;
+    }
+  };
+
+  const changeName = (value) => {
+    setName(value);
+    if (value.length == 0) {
+      setNameError("이름을 입력하세요.");
+    } else if (value.length > 20) {
+      setNameError("이름은 최대 20글자 입니다.");
+    } else {
+      setNameError(null);
+      return true;
+    }
+  };
+
+  const changeSchool = (value) => {
+    setSchool(value);
+    setSchoolLock(false);
+    setSchoolList([]);
+  };
+
+  const selectSchool = (li) => {
+    return () => {
+      setSchoolList([]);
+      setSchool(li);
+      setSchoolLock(true);
+    };
+  };
+
+  const searchSchool = () => {
+    if (schoolLoading) return;
+    setSchoolLoading(true);
+    setSchoolLock(false);
+    setSchoolList([{ label: "로딩 중..", click: null }]);
+
+    fetch("/school", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        school: school,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (!res.result) {
+          setSchoolList([{ label: `로드 실패 : ${res.reason}`, click: null }]);
+          // setSchoolList([{ label: "로드 실패 방지용 임시학교", click: selectSchool("로드 실패 방지용 임시학교") }]);
+          return;
         }
-    }
 
-    const changeName = (value) => {
-        setName(value);
-        if (value.length == 0) {
-            setNameError("이름을 입력하세요.");
-        } else if (value.length > 20) {
-            setNameError("이름은 최대 20글자 입니다.");
-        } else {
-            setNameError(null);
-            return true;
+        if (res.data.length == 0) {
+          setSchoolList([{ label: "학교를 찾을 수 없었어요..", click: null }]);
+          return;
         }
+
+        setSchoolList(
+          res.data.map((li) => ({
+            label: li,
+            click: selectSchool(li),
+          }))
+        );
+      })
+      .finally(() => {
+        setSchoolLoading(false);
+      });
+  };
+
+  const checkSchool = () => {
+    if (!schoolLock) {
+      setSchoolError("학교를 선택해주세요.");
+    } else {
+      setSchoolError(null);
+      return true;
     }
+  };
 
-    const changeSchool = (value) => {
-        setSchool(value);
-        setSchoolLock(false);
-        setSchoolList([]);
+  const submit = () => {
+    let gradeOk = changeGrade(grade);
+    let nameOk = changeName(name);
+    let schoolOk = checkSchool();
+
+    //if (!gradeError && !nameError && !schoolError) {
+    if (gradeOk && nameOk && schoolOk) {
+      setFormFilled(true);
+      setStartTime(Date.now());
     }
+  };
 
-    const selectSchool = (li) => {
-        return () => {
-            setSchoolList([]);
-            setSchool(li);
-            setSchoolLock(true);
-        }
-    }
-
-    const searchSchool = () => {
-        if (schoolLoading) return;
-        setSchoolLoading(true);
-        setSchoolLock(false);
-        setSchoolList([{ label: "로딩 중..", click: null }]);
-
-        fetch("/school", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                school: school
-            })
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            if (!res.result) {
-                // setSchoolList([{ label: `로드 실패 : ${res.reason}`, click: null }]);
-                setSchoolList([{ label: "로드 실패 방지용 임시학교", click: selectSchool("로드 실패 방지용 임시학교") }]);
-                return;
-            }
-
-            if (res.data.length == 0) {
-                setSchoolList([{ label: "학교를 찾을 수 없었어요..", click: null }]);
-                return;
-            }
-
-            setSchoolList(res.data.map((li) => ({
-                label: li,
-                click: selectSchool(li)
-            })));
-        })
-        .finally(() => {
-            setSchoolLoading(false);
-        });
-    }
-
-    const checkSchool = () => {
-        if (!schoolLock) {
-            setSchoolError("학교를 선택해주세요.");
-        } else {
-            setSchoolError(null);
-            return true;
-        }
-    }
-
-    const submit = () => {
-        let gradeOk = changeGrade(grade);
-        let nameOk = changeName(name);
-        let schoolOk = checkSchool();
-
-        //if (!gradeError && !nameError && !schoolError) {
-        if (gradeOk && nameOk && schoolOk) {
-            setFormFilled(true);
-            setStartTime(Date.now());
-        }
-    }
-
-    return (
-        <div className='form_frame'>
-            <div className='form_title'>창의경영고 '1분 1초' 설문</div>
-            <p className='form_desc'>학급 정보를 입력한 뒤, 창의경영고 일학습병행 홍보 영상 '1분 1초'를 30초 이상 시청해주세요.</p>
-            <form>
-                <InputLine label="학번" placeholder="학번을 입력하세요." type="number" value={grade} error={gradeError} onChange={changeGrade} />
-                <InputLine label="이름" placeholder="이름을 입력하세요." type="text" value={name} error={nameError} onChange={changeName} />
-                <InputList label="학교명" placeholder="학교명을 입력하세요." type="text" value={school} error={schoolError} list={schoolList} onChange={changeSchool} onClick={searchSchool} />
-                <button className='submit' type='button' onClick={submit}>제출하고 시청하기</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="form_frame">
+      <div className="form_title">창의경영고 '1분 1초' 설문</div>
+      <p className="form_desc">
+        학급 정보를 입력한 뒤, 창의경영고 일학습병행 홍보 영상 '1분 1초'를 30초
+        이상 시청해주세요.
+      </p>
+      <form>
+        <InputLine
+          label="학번"
+          placeholder="학번을 입력하세요."
+          type="number"
+          value={grade}
+          error={gradeError}
+          onChange={changeGrade}
+        />
+        <InputLine
+          label="이름"
+          placeholder="이름을 입력하세요."
+          type="text"
+          value={name}
+          error={nameError}
+          onChange={changeName}
+        />
+        <InputList
+          label="학교명"
+          placeholder="학교명을 입력하세요."
+          type="text"
+          value={school}
+          error={schoolError}
+          list={schoolList}
+          onChange={changeSchool}
+          onClick={searchSchool}
+        />
+        <button className="submit" type="button" onClick={submit}>
+          제출하고 시청하기
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const CryptoJS = require("crypto-js");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // const config = path.join(__dirname, "config/db.json");
 // const connection = mysql.createConnection(
@@ -20,101 +20,122 @@ const apiConfigPath = path.join(__dirname, "config/api.json");
 const apiConfig = JSON.parse(fs.readFileSync(apiConfigPath));
 
 async function query(queryStr, params) {
-    const [rows, fields] = await connection.promise().query(queryStr, params);
-    return rows;
+  const [rows, fields] = await connection.promise().query(queryStr, params);
+  return rows;
 }
 
 const client = path.join(__dirname, "../client/build");
 app.get("/app", (req, res) => {
-    res.sendFile(client + "/index.html");
-})
+  res.sendFile(client + "/index.html");
+});
 
 app.post("/school", (req, res) => {
-    let school = req.body.school;
-    if (typeof school != "string") {
-        res.json({
-            result: false,
-            reason: "옳바르지 않은 데이터 타입입니다."
-        });
-        return;
-    }
+  let school = req.body.school;
+  if (typeof school != "string") {
+    res.json({
+      result: false,
+      reason: "옳바르지 않은 데이터 타입입니다.",
+    });
+    return;
+  }
 
-    const headers = { "Content-Type": "application/json" }
-    axios.get(`${apiConfig.url}?serviceKey=${apiConfig.key}&type=json&pageNo=1&numOfRows=100&schoolNm=${school}`, {headers: headers})
+  const headers = { "Content-Type": "application/json" };
+  axios
+    .get(
+      `${apiConfig.url}?KEY=${apiConfig.key}&Type=json&pIndex=1&pSize=20&SCHUL_NM=${school}`,
+      { headers: headers }
+    )
     .then((apiRes) => apiRes.data)
     .then((apiRes) => {
-        apiRes = apiRes.response;
-        console.log(apiRes);
-        if (apiRes.body) {
-            res.json({
-                result: true,
-                data: apiRes.body
-            });
-        } else {
-            res.json({
-                result: false,
-                reason: `fetch 실패 - ${apiRes.header.resultMsg}`
-            });
-        }
-    }).catch((err) => {
+      if (apiRes.schoolInfo) {
+        let schools = apiRes.schoolInfo[1].row;
+        schools = schools.reduce((a, c) => [...a, c.SCHUL_NM], []);
+
         res.json({
-            result: false,
-            reason: `fetch 오류 - ${err}`
+          result: true,
+          data: schools,
         });
+      } else {
+        if (apiRes.RESULT.CODE == "INFO-200") {
+          res.json({ result: true, data: [] });
+        } else {
+          res.json({
+            result: false,
+            reason: `fetch 실패 - ${apiRes.RESULT.MESSAGE}`,
+          });
+        }
+      }
     })
-})
+    .catch((err) => {
+      res.json({
+        result: false,
+        reason: `fetch 오류 - ${err}`,
+      });
+    });
+});
 
 app.post("/done", (req, res) => {
-    let grade = req.body.grade;
-    let name = req.body.name;
-    let school = req.body.school;
-    let secret = req.body.secret;
+  let grade = req.body.grade;
+  let name = req.body.name;
+  let school = req.body.school;
+  let secret = req.body.secret;
 
-    if (typeof grade != "string" || typeof name != "string" || typeof school != "string" || typeof secret != "string") {
-        res.json({
-            result: false,
-            reason: "옳지 않은 형식입니다!"
-        });
-        return;
-    }
+  if (
+    typeof grade != "string" ||
+    typeof name != "string" ||
+    typeof school != "string" ||
+    typeof secret != "string"
+  ) {
+    res.json({
+      result: false,
+      reason: "옳지 않은 형식입니다!",
+    });
+    return;
+  }
 
-    if (grade.length != 5 || isNaN(grade) || !(name.length > 0 && name.length <= 20) || school.length == 0 || secret.length == 0) {
-        res.json({
-            result: false,
-            reason: "옳지 않은 형식입니다!"
-        });
-        return;
-    }
-    grade = Number(grade);
+  if (
+    grade.length != 5 ||
+    isNaN(grade) ||
+    !(name.length > 0 && name.length <= 20) ||
+    school.length == 0 ||
+    secret.length == 0
+  ) {
+    res.json({
+      result: false,
+      reason: "옳지 않은 형식입니다!",
+    });
+    return;
+  }
+  grade = Number(grade);
 
-    const key = "gmma";
-    secret = CryptoJS.AES.decrypt(secret, key);
-    try {
-        secret = secret.toString(CryptoJS.enc.Utf8);
-        if (isNaN(secret)) new Error("숫자가 아닙니다!");
-    } catch (e) {
-        res.json({
-            result: false,
-            reason: "잘못된 코드입니다."
-        })
-        return;
-    }
+  const key = "gmma";
+  secret = CryptoJS.AES.decrypt(secret, key);
+  try {
+    secret = secret.toString(CryptoJS.enc.Utf8);
+    if (isNaN(secret)) new Error("숫자가 아닙니다!");
+  } catch (e) {
+    res.json({
+      result: false,
+      reason: "잘못된 코드입니다.",
+    });
+    return;
+  }
 
-    if (secret <= 28000) {
-        res.json({
-            result: false,
-            reason: "영상을 30초 이상 시청하지 않았습니다."
-        })
-        return;
-    }
+  if (secret <= 28000) {
+    res.json({
+      result: false,
+      reason: "영상을 30초 이상 시청하지 않았습니다.",
+    });
+    return;
+  }
 
-    // DB에 저장하는 코드 추가하기
-    console.log(`${school}의 ${grade} ${name}님이 영상 시청을 완료했습니다.`);
-    res.json({ result: true });
-})
+  // DB에 저장하는 코드 추가하기
+  console.log(`${school}의 ${grade} ${name}님이 영상 시청을 완료했습니다.`);
+  res.json({ result: true });
+});
 
 app.use(express.static(client));
 
 app.listen(8888, () => {
-    console.log("Server activated on port 8888");
+  console.log("Server activated on port 8888");
 });
